67fc7098b6aa728f4751fa0335a6d73e774d3b87
Ricardo Marquez <rikmarqk@gmail.com>
2020-09-14 19:43:30 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,13 +1,5 @@
-current_time = Time.now
-current_time = current_time.to_i
-
-if current_time % 2 == 0
+if Time.now.to_i % 2 == 0
   puts "Even!"
 else
   puts "Odd!"
-end
-
-result = 2 + 2
-if result == 4
-  puts "We've done it!"
 end
