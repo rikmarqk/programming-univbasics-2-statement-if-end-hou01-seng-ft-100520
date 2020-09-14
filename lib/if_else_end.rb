cd56397796a32ac6de2fe18ec911dd00c9312e22
Ricardo Marquez <rikmarqk@gmail.com>
2020-09-14 19:52:09 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,28 @@
-# Write your solution here
+current_time = Time.now
+current_time = current_time.to_i
+
+if current_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
+
+if Time.now.to_i % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
+
+current_time = Time.now
+current_time = current_time.to_i
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
+
+result = 2 + 2
+
+if result == 4
+  puts "We've done it!"
+end
